ea7de37fac7fee3042e3d58238a9def6ec5afee7
lukgrz <lukaszgrzegorzak@gmail.com>
2021-09-03 11:55:15 +0200

    19.3.1 Delete task

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -30,7 +30,7 @@ public class TaskController {
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
-    public void deleteTask(Long taskId) {
+    public void deleteTask(@RequestParam Long taskId) {
         service.deleteTask(taskId);
     }
 
--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -18,9 +18,4 @@ public interface TaskRepository extends CrudRepository<Task, Long> {
 
     @Override
     Optional<Task> findById(Long id);
-
-    @Override
-    void deleteById(Long id);
-
-
 }
