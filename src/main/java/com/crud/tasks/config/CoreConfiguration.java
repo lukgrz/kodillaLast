ae3e1e3b6bee05dfb072f530663fa1ed6b5d99a6
lukgrz <lukaszgrzegorzak@gmail.com>
2021-09-22 00:20:19 +0200

    23.2 Swagger exercise

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -22,8 +22,7 @@ public class CoreConfiguration {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
                 .build();
     }
 }
