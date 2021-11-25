d02f8d8547600a9779c72c525bb6779893918cb0
lukgrz <lukaszgrzegorzak@gmail.com>
2021-11-25 19:35:54 +0100

    32.2 Thymeleaf - Creating templates.

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -3,40 +3,47 @@ package com.crud.tasks.service;
 import com.crud.tasks.domain.Mail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class SimpleEmailService {
 
+    @Autowired
+    private MailCreatorService mailCreatorService;
     private final JavaMailSender javaMailSender;
 
     public void send(final Mail mail) {
         log.info("Preparing email to send....");
         try {
-            SimpleMailMessage mailMessage = createMailMessage(mail);
-            javaMailSender.send(mailMessage);
-            log.info("Email has been sent");
+            javaMailSender.send(createMimeMessage(mail));
+            log.info("Email has been sent.");
         } catch (MailException e) {
             log.info("Failed to send an email:" + e.getMessage() + e.getCause(), e);
         }
     }
 
-    private  SimpleMailMessage createMailMessage(final Mail mail) {
+    private MimeMessagePreparator createMimeMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
+    }
+
+
+    private SimpleMailMessage createMailMessage(final Mail mail) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
-        mailMessage.setSubject(mail.getSubject());
-        mailMessage.setText(mail.getMessage());
-        Optional<String> toCc = Optional.ofNullable(mail.getToCc());
-        if (toCc.isPresent()) {
-             mailMessage.setCc(toCc.get());
-        }
+        mailMessage.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()));
         return mailMessage;
     }
 }
